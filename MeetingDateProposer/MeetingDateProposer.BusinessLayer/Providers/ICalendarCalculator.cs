25e626f6084d0e93cf1b0532a4b9345f2c798ede
Vlad Gavrilchik <v.gavrilchik@itransition.com>
2021-04-22 21:52:42 +0300

    Hotfix after review

--- MeetingDateProposer/MeetingDateProposer.BusinessLayer/Providers/ICalendarCalculator.cs
+++ MeetingDateProposer/MeetingDateProposer.BusinessLayer/Providers/ICalendarCalculator.cs
@@ -1,15 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using MeetingDateProposer.DataLayer;
-using MeetingDateProposer.Domain.Models;
+﻿using MeetingDateProposer.Domain.Models;
 
 namespace MeetingDateProposer.BusinessLayer.Providers
 {
     public interface ICalendarCalculator
     {
-        public Calendar CalculateAvailableMeetingTime(Meeting currentMeeting)
-        {
-            return new Calendar();
-        }
+        public Calendar CalculateAvailableMeetingTime(Meeting currentMeeting);
     }
 }
--- MeetingDateProposer/MeetingDateProposer.BusinessLayer/Providers/ICalendarProvider.cs
+++ MeetingDateProposer/MeetingDateProposer.BusinessLayer/Providers/ICalendarProvider.cs
@@ -1,18 +1,10 @@
-﻿using System.Dynamic;
-using System.Runtime.InteropServices.ComTypes;
-using System.Collections.Generic;
-using System.Linq;
-using MeetingDateProposer.DataLayer;
-using MeetingDateProposer.Domain.Models;
+﻿using MeetingDateProposer.Domain.Models;
 
 namespace MeetingDateProposer.BusinessLayer.Providers
 {
     public  interface ICalendarProvider
     {
-        public void GetCalendar(User user)
-        {
-            
-        }
+        public void GetCalendar(User user);
 
     }
 }
